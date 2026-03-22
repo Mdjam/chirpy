4a6a4c1ef6dde5dd99d147152ed6f0dd52652c44
mdjam <malo.desjam@gmail.com>
2026-03-22 17:50:01 +0100

    added requests handlers for app/ & healthz/

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 
 const port = "8080"
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := http.Server {
 		Addr: ":" + port,
