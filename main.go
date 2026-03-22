29b936996c2176a6eeac4fc138a2040a70900ed3
mdjam <malo.desjam@gmail.com>
2026-03-22 18:34:38 +0100

    added handlers & middleware for metrics

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port = "8080"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (a *apiConfig) metricsIncMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (a *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", a.fileserverHits.Load())))
+}
+
+func (a *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	a.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Metrics have been reset"))
+}
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -14,10 +40,13 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.metricsIncMiddleware(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.metricsResetHandler)
 
 	server := http.Server {
 		Addr: ":" + port,
